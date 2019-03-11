eafbec90a12f61c44801927836a4db07058c19d8
Florian Heilmann <heilmann@eit.uni-kl.de>
2019-03-11 10:08:42 +0100

    First draft
    Signed-off-by: Florian Heilmann <heilmann@eit.uni-kl.de>

---
+++ mensa.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import locale
+import urllib.request
+from bs4 import BeautifulSoup
+from colorama import Style
+from datetime import datetime
+from contextlib import contextmanager
+
+
+@contextmanager
+def setlocale(name):
+    saved = locale.setlocale(locale.LC_ALL)
+    try:
+        yield locale.setlocale(locale.LC_ALL, name)
+    finally:
+        locale.setlocale(locale.LC_ALL, saved)
+
+with urllib.request.urlopen("https://www.studierendenwerk-kaiserslautern.de/kaiserslautern/essen-und-trinken/tu-kaiserslautern/mensa/") as response:
+    page =  response.read()
+page_soup = BeautifulSoup(page, "html.parser")
+daily_contents = page_soup.findAll('div', {'class' : 'dailyplan_content'})
+
+for daily_content in daily_contents:
+    with setlocale('de_DE.UTF-8'):
+        date = datetime.strptime(daily_content.h5.text, '%A, %d.%m.%Y')
+    if date.date() == datetime.today().date():
+        print(Style.BRIGHT)
+    print("=======================================\n")
+    print(daily_content.h5.text)
+    rows = daily_content.findAll('div', {'class': 'subcolumns'})
+    for row in rows:
+        location = row.find('div', {'class': 'subcl'})
+        foods = row.find('div', {'class': 'subcr'})
+        food = foods.find_all('strong')
+        foodlist = []
+        for fd in foods:
+            try:
+                if fd.text:
+                    foodlist.append(fd.text)
+            except AttributeError:
+                pass
+
+        print('\t'.join([location.strong.text] + ['\n\t'.join(foodlist)]))
+    print(Style.RESET_ALL)
+print("=======================================")
