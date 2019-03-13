c7d6dcfa2a80699d6e11b839efe6889cf4fcd35b
Florian Heilmann <heilmann@eit.uni-kl.de>
2019-03-13 15:00:13 +0100

    Add caching

--- mensa.py
+++ mensa.py
@@ -1,37 +1,110 @@
 #!/usr/bin/env python3
-
-import locale
+"""Terminal application to grab and display the food plan of
+   the TU KL mensa
+"""
+import os
+import re
+import glob
 import urllib.request
-from bs4 import BeautifulSoup
-from colorama import Style
 from datetime import datetime
 from contextlib import contextmanager
 
-locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
+import click
+from bs4 import BeautifulSoup
+from colorama import Style, Fore
 
-with urllib.request.urlopen("https://www.studierendenwerk-kaiserslautern.de/kaiserslautern/essen-und-trinken/tu-kaiserslautern/mensa/") as response:
-    page =  response.read()
-page_soup = BeautifulSoup(page, "html.parser")
-daily_contents = page_soup.findAll('div', {'class' : 'dailyplan_content'})
+MENSA_URL = "https://www.studierendenwerk-kaiserslautern.de/kaiserslautern" + \
+            "/essen-und-trinken/tu-kaiserslautern/mensa/"
 
-for daily_content in daily_contents:
-    date = datetime.strptime(daily_content.h5.text, '%A, %d.%m.%Y')
-    if date.date() == datetime.today().date():
-        print(Style.BRIGHT)
-    print("=======================================")
-    print(daily_content.h5.text)
-    rows = daily_content.findAll('div', {'class': 'subcolumns'})
-    for row in rows:
-        location = row.find('div', {'class': 'subcl'})
-        foods = row.find('div', {'class': 'subcr'})
-        food = foods.find_all('strong')
-        foodlist = []
-        for fd in foods:
-            try:
-                if fd.text:
-                    foodlist.append(fd.text)
-            except AttributeError:
-                pass
+CACHE_EXT = '.mensacache'
 
-        print('\t'.join([location.strong.text] + ['\n\t'.join(foodlist)]))
-    print("=======================================" + Style.RESET_ALL)
+@contextmanager
+def day_block(is_today=False):
+    """Prints a day block in between two separator lines
+
+    Keyword Arguments:
+        is_today {bool} -- Whether to make the block bold or not (default: {False})
+    """
+    style = Style.BRIGHT if is_today else ''
+    print(style + "="*40)
+    try:
+        yield
+    finally:
+        print("="*40 + Style.RESET_ALL)
+
+
+def get_mensakl_page():
+    """Get the mensa page. The first request per boot and/or week
+       is cached in /tmp to avoid repetitive requests.
+
+    Returns:
+        page contents, either served from a web request or from the
+        cached file
+    """
+
+    datestring = datetime.today().strftime("%Y_%U")
+    for file_cache in glob.glob('/tmp/*'+ CACHE_EXT):
+        if file_cache == "/tmp/"+ datestring + CACHE_EXT:
+            with open(file_cache, 'r') as page:
+                content = page.read()
+            return content
+        else:
+            os.remove(file_cache)
+    with urllib.request.urlopen(MENSA_URL) as response:
+        page = response.read()
+    with open("/tmp/"+datestring+CACHE_EXT, 'w') as file_cache:
+        file_cache.write(page.decode('utf-8'))
+    return page
+
+
+def print_day(daily_content, is_today=False, vegetarian=False):
+    """[summary]
+
+    Arguments:
+        daily_content -- HTML DOM of one day
+
+    Keyword Arguments:
+        is_today {bool} -- Whether or not the day in question is today (default: {False})
+    """
+
+    with day_block(is_today):
+        print(daily_content.h5.text)
+        rows = daily_content.findAll('div', {'class': 'subcolumns'})
+        for row in rows:
+            location = row.find('div', {'class': 'subcl'})
+            foods = row.find('div', {'class': 'subcr'})
+            foodlist = []
+            for food in foods:
+                try:
+                    if food.text:
+                        if re.findall(r',V,|\(V|V\)',food.text):
+                            foodlist.append(Fore.GREEN + food.text + Fore.RESET)
+                        elif not vegetarian:
+                            foodlist.append(food.text)
+                except AttributeError:
+                    pass
+            if foodlist:
+                print('\t'.join([location.strong.text] + ['\n\t'.join(foodlist)]))
+
+@click.command()
+@click.option('--today', is_flag=True, help="List the food for the current day")
+@click.option('--week', is_flag=True, help="List the food for the current week")
+@click.option('--vegetarian', is_flag=True, help="Only list vegetarian options")
+def mensa(today: bool = False, week: bool = False, vegetarian: bool = False):
+    """Query the webpage of the TUKL mensa and print the daily plans to the terminal
+    """
+
+    if not week and not today:
+        click.echo(click.get_current_context().get_help())
+        return
+    page_soup = BeautifulSoup(get_mensakl_page(), "html.parser")
+    for day in page_soup.findAll('div', {'class' : 'dailyplan_content'}):
+        date_text = re.findall(r'\d+.\d+.\d+', day.h5.text)[0]
+        date = datetime.strptime(date_text, '%d.%m.%Y')
+        if date.date() == datetime.today().date() and (week or today):
+            print_day(day, is_today=True, vegetarian=vegetarian)
+        elif week:
+            print_day(day, is_today=False, vegetarian=vegetarian)
+
+if __name__ == '__main__':
+    mensa()
