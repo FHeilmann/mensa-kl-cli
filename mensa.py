736642c2cf8d0fffb3eafca316753a5b270d2f5d
Florian Heilmann <heilmann@eit.uni-kl.de>
2019-03-15 15:41:30 +0100

    Add loading spinners

--- mensa.py
+++ mensa.py
@@ -10,6 +10,7 @@ from datetime import datetime
 from contextlib import contextmanager
 
 import click
+from halo import Halo
 from bs4 import BeautifulSoup
 from colorama import Style, Fore
 
@@ -18,20 +19,44 @@ MENSA_URL = "https://www.studierendenwerk-kaiserslautern.de/kaiserslautern" + \
 
 CACHE_EXT = '.mensacache'
 
-@contextmanager
-def day_block(is_today=False):
-    """Prints a day block in between two separator lines
+spinner = Halo(spinner='line')
 
-    Keyword Arguments:
-        is_today {bool} -- Whether to make the block bold or not (default: {False})
+@contextmanager
+def day_block():
+    """Prints a day block in between two separator lines
     """
-    style = Style.BRIGHT if is_today else ''
-    print(style + "="*40)
+    print("="*40)
     try:
         yield
     finally:
         print("="*40 + Style.RESET_ALL)
 
+# @Halo(text="Trying to load mensa-kl content from cache ...", spinner="line")
+def get_page_from_cache(datestring):
+    spinner.start('Trying to load mensa-kl content from cache ...')
+    for file_cache in glob.glob('/tmp/*'+ CACHE_EXT):
+        if file_cache == "/tmp/"+ datestring + CACHE_EXT:
+            with open(file_cache, 'r') as page:
+                content = page.read()
+            spinner.stop_and_persist(symbol=Fore.GREEN+u"\u2713"+Fore.RESET, text="Found cached mensa-kl content!")
+            return content
+        else:
+            os.remove(file_cache)
+    spinner.stop_and_persist(symbol=Fore.RED+u"\u2717"+Fore.RESET, text="No cached mensa-kl content found!")
+    return None
+
+# @Halo(text="Trying to load mensa-kl content from web ...", spinner="line")
+def get_page_from_web(datestring):
+    spinner.start('Trying to load mensa-kl content from web ...')
+    with urllib.request.urlopen(MENSA_URL) as response:
+        if response.getcode() is not 200:
+            spinner.stop_and_persist(symbol=Fore.RED+u"\u2717"+Fore.RESET, text="Error while loading mensa-kl webpage!")
+            return None
+        page = response.read()
+    with open("/tmp/"+datestring+CACHE_EXT, 'w') as file_cache:
+        file_cache.write(page.decode('utf-8'))
+    spinner.stop_and_persist(symbol=Fore.GREEN+u"\u2713"+Fore.RESET, text="Successfully downloaded mensa-kl webpage!")
+    return page
 
 def get_mensakl_page():
     """Get the mensa page. The first request per boot and/or week
@@ -43,19 +68,11 @@ def get_mensakl_page():
     """
 
     datestring = datetime.today().strftime("%Y_%U")
-    for file_cache in glob.glob('/tmp/*'+ CACHE_EXT):
-        if file_cache == "/tmp/"+ datestring + CACHE_EXT:
-            with open(file_cache, 'r') as page:
-                content = page.read()
-            return content
-        else:
-            os.remove(file_cache)
-    with urllib.request.urlopen(MENSA_URL) as response:
-        page = response.read()
-    with open("/tmp/"+datestring+CACHE_EXT, 'w') as file_cache:
-        file_cache.write(page.decode('utf-8'))
-    return page
-
+    content = get_page_from_cache(datestring)
+    if not content:
+        return get_page_from_web(datestring)
+    else:
+        return content
 
 def print_day(daily_content, is_today=False, vegetarian=False):
     """[summary]
@@ -67,8 +84,8 @@ def print_day(daily_content, is_today=False, vegetarian=False):
         is_today {bool} -- Whether or not the day in question is today (default: {False})
     """
 
-    with day_block(is_today):
-        print(daily_content.h5.text)
+    with day_block():
+        print((Style.BRIGHT if is_today else "") + daily_content.h5.text + Style.RESET_ALL)
         rows = daily_content.findAll('div', {'class': 'subcolumns'})
         for row in rows:
             location = row.find('div', {'class': 'subcl'})
@@ -92,7 +109,12 @@ def print_day(daily_content, is_today=False, vegetarian=False):
 def mensa(week: bool = False, vegetarian: bool = False):
     """Query the webpage of the TUKL mensa and print the daily plans to the terminal
     """
-    page_soup = BeautifulSoup(get_mensakl_page(), "html.parser")
+    page = get_mensakl_page()
+    if not page:
+        print("ERROR: Could not find mensa kl page in cache, and couldn't" +
+              "obtain page from web!")
+        return
+    page_soup = BeautifulSoup(page, "html.parser")
     for day in page_soup.findAll('div', {'class' : 'dailyplan_content'}):
         date_text = re.findall(r'\d+.\d+.\d+', day.h5.text)[0]
         date = datetime.strptime(date_text, '%d.%m.%Y')
